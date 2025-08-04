f1a837fffbffe06c4451f45d1ecf718d52b5e0ac
Stefano Piva <stefano.piva9@gmail.com>
2025-08-04 14:07:38 +0200

    Remove js files from tests

--- test/unit/index.test.js
+++
@@ -1,7 +0,0 @@
-// src/index.test.ts
-import { describe, it, expect } from 'vitest';
-describe('CLI Greeting', () => {
-  it('should return the correct greeting message', () => {
-    expect('Hello, CLI!').toBe('Hello, CLI!');
-  });
-});
